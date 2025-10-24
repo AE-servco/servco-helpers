3384c704e2a87a801abca2842d289404eda8f1c0
Albie Ewins <albie.ewins@tradesaus.com.au>
2025-10-24 15:12:19 +1100

    removed redundant files

--- servco_helpers/gcp_utils.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import secretmanager
-
-def get_secret(secret_id, project_id="servco1", version_id="latest"):
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-    response = client.access_secret_version(request={"name": name})
-    secret_payload = response.payload.data.decode("UTF-8")
-    return secret_payload
